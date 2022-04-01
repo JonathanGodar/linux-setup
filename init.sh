f1f007fdb9b495040468902b62c7100fd9c038c5
JonathanGodar <jonathan.godar@gmail.com>
2022-04-02 00:55:30 +0200

    Continuation

--- init.sh
+++ init.sh
@@ -2,7 +2,34 @@
 
 
 archSetup() {
-	echo "hello"
+	sudo pacman -S git
+
+	installYay
+	cat ./packages/packages.txt | grep -v ^# | xargs | yay -S
+
+	updateConfigurations
 }
 
+updateConfigurations(){
+	cd ~
+	git clone https://github.com/JonathanGodar/.dotfiles
+	cd .dotfiles
+
+	stow -S *
+}
+
+updateConfigurations
+
+installYay(){
+	# Required for installing yay
+	sudo pacman -S base-devel
+
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+
+	cd ../
+}
+
+script_path=`pwd`
 archSetup
